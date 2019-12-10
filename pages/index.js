04ccc03581cba5253134ca29950173da6e39c4ff
ofersarid <ofersarid@gmail.com>
2019-12-10 17:10:58 +0200

    fixed bug

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,16 @@ import styles from './styles.scss';
 import { device } from '../services';
 import { Desktop, Mobile } from '../containers';
 
-let prevFrame = 0;
+let prevFrame = 1;
 
 const Index = ({ router, isServer }) => {
   // const data = useSelector(state => reactor.selectors.pageData(state, 'test page'));
   const isMobile = useSelector(device.selectors.type) === 'mobile';
   const frame = parseInt(router.query.frame);
-  const forward = frame > prevFrame;
-  const animation = (frame !== prevFrame || isServer) ? {
-    [styles.comeUp]: forward || isServer,
-    [styles.comeDown]: !forward && !isServer
+  const forward = frame >= prevFrame;
+  const animation = frame !== prevFrame ? {
+    [styles.comeUp]: forward,
+    [styles.comeDown]: !forward
   } : {};
   prevFrame = frame;
   return isMobile ? <Mobile animation={animation} forward={forward} frame={frame} />
